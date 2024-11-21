0c3c4fab974e51addc63e09a0a8189656f1d3f4c
VictorMundia <victor.mundia@strathmore.edu>
2024-11-21 12:05:58 +0300

    Check Constraints and Tempral triggers

--- 4_check_constraints/script.sql
+++ 4_check_constraints/script.sql
@@ -1,1 +1,53 @@
 -- Write your SQL code here
+Equipment Table
+CREATE TABLE Equipment (
+    equipmentID VARCHAR(25) PRIMARY KEY,
+    manufacturer VARCHAR(50),
+    purchaseDate DATE CHECK (purchaseDate <= CURRENT_DATE)    lifespan VARCHAR(50),
+    status CHAR(20) CHECK (status IN ('Operational', 'Not operational', 'Scheduled for maintenance', 'Decommissioned')) 
+) ENGINE=InnoDB; 
+CREATE INDEX idx_status ON Equipment(status);
+
+Maintenance table
+CREATE TABLE Maintenance (
+    sessionID VARCHAR(25) PRIMARY KEY, 
+    equipmentID VARCHAR(25),
+    scheduledDate TIMESTAMP CHECK (scheduledDate >= CURRENT_TIMESTAMP),     actualDate TIMESTAMP,
+    status CHAR(15) DEFAULT 'Pending' CHECK (status IN ('Pending', 'Completed', 'Cancelled')),    FOREIGN KEY (equipmentID) REFERENCES Equipment(equipmentID)
+) ENGINE=InnoDB;
+CREATE INDEX idx_equipment ON Maintenance(equipmentID);
+
+Spare Parts Table
+CREATE TABLE SpareParts (
+    partID VARCHAR(25) PRIMARY KEY, 
+    partName VARCHAR(50),
+    manufacturer VARCHAR(50),
+    quantity INT UNSIGNED CHECK (quantity >= 0),
+    partCost DECIMAL(10, 2) CHECK (partCost > 0), 
+    equipmentID VARCHAR(25),
+    FOREIGN KEY (equipmentID) REFERENCES Equipment(equipmentID)
+) ENGINE=MyISAM;
+CREATE INDEX idx_manufacturer ON SpareParts(manufacturer);
+
+Downtime Table
+CREATE TABLE Downtime (
+    downtimeID INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    equipmentID VARCHAR(25),
+    downtimeDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    duration TIME CHECK (duration > '00:00:00'), 
+    failureDescription VARCHAR(100) CHECK (LENGTH(failureDescription) <= 100)
+    FOREIGN KEY (equipmentID) REFERENCES Equipment(equipmentID)
+) ENGINE=InnoDB;
+CREATE INDEX idx_downtime ON Downtime(equipmentID, downtimeDate);
+
+Finance Records Table
+CREATE TABLE FinanceRecord (
+    recordID VARCHAR(25) PRIMARY KEY,
+    downtimeID INT,
+    maintenanceCost DECIMAL(10, 2) CHECK (maintenanceCost >= 0)
+    downtimeCost DECIMAL(10, 2) CHECK (downtimeCost >= 0),
+    FOREIGN KEY (downtimeID) REFERENCES Downtime(downtimeID)
+) ENGINE=InnoDB;
+CREATE INDEX idx_downtime_id ON FinanceRecord(downtimeID);
+
+
